d99e3c679a49a6e7c756a0c28582a15434d32dab
shivamchadha <shivamchadha.sc@gmail.com>
2018-01-13 01:52:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-exports.helloworld = function helloworld(req,res){
-	//example input: {"Hi"}
-    if (req.body.message ===undefined){
-    	res.status(400).send('Hello World');
-     } else {
-     	console.log(req.body.message);
-     	res.status(200).send('Success:' + req.body.message);
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
 
-     }
-    
-};
+// The Firebase Admin SDK to access the Firebase Realtime Database. 
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.(
+    if (req.body.message === undefined){
+    	res.status(400).send('Hello World');
+    }else{console.log(req.body.message);
+    	res.status(200).send(req.body.message);}
+  const original = req.query.text;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  admin.database().ref('/messages').push({original: original}).then(snapshot => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    res.redirect(303, snapshot.ref);
+  });
+});
